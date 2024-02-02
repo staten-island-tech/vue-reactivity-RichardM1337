1857be2fd3712c3e552a47bfc82ed8aa9cfd13f3
xX_EpicHackerDude_Xx <113366399+RichardM1337@users.noreply.github.com>
2024-02-02 13:14:43 -0500

    wdwd

---
+++ vue-project/src/components/TheWelcome.vue
@@ -0,0 +1,15 @@
+<!-- install vue language features && vscode snippets -->
+<!-- consider tailwind-->
+<template>
+  <div>
+    <h1 v-if="answer">correct</h1>
+    <h2 v-else>incorrect</h2>
+    <h2 v-for="animal in animals" :key="animal">{{ animal }}</h2>
+  </div>
+</template>
+
+<script setup>
+const answer = true;
+const animals = ["Ape", "Baboon", "Elk"];
+</script>
+<style scoped></style>
