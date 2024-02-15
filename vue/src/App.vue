4d55021ae1fa68eade688ac46956ad134232eaa5
xX_EpicHackerDude_Xx <113366399+RichardM1337@users.noreply.github.com>
2024-02-15 13:32:12 -0500

    wdwdw

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,9 +1,14 @@
 <script setup>
 import { RouterLink, RouterView, useRouter, useRoute } from "vue-router";
+import { reactive, computed } from "vue";
 AOS.init();
+const router = useRouter();
+const route = useRoute();
 function cartPage() {
-  router.push("/cart");
+  const currentRouteName = router.currentRoute.value.path;
+  return currentRouteName == "/" ? router.push("/cart") : router.push("/");
 }
+// computed only changes on dependencies -- so less getter acccess
 </script>
 
 <template>
@@ -20,5 +25,12 @@ function cartPage() {
   <transition name="fade">
     <component :is="Component" />
   </transition>
-  <button @click="cartPage">^^^</button>
+  <footer>
+    <button
+      class="flex justify-center text-center sticky left-1/2 bottom-20px m-0 translate-x-1/2 translate-y-1/2"
+      @click="cartPage"
+    >
+      ^^^
+    </button>
+  </footer>
 </template>
